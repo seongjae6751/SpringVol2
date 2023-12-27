56befe86c10f14d8dc06522de50213ce53f611c7
김성재 <103095432+seongjae6751@users.noreply.github.com>
2023-12-27 12:44:11 +0900

    Apply .gitignore

--- src/main/java/com/api/MvcApi/DBConnection/DBconnectionConstant.java
+++
@@ -1,7 +0,0 @@
-package com.api.MvcApi.DBConnection;
-
-public class DBconnectionConstant {
-    public static final String URL = "jdbc:mysql://localhost:3306/community_user";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "0000";
-}
