9df0e7cb9ca9331c948da1646cd9a0f35adb0764
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-01 23:56:13 +0900

    JDBC change to JPA

--- src/main/java/com/api/MvcApi/service/UserService.java
+++ src/main/java/com/api/MvcApi/service/UserService.java
@@ -1,19 +1,20 @@
-//package com.api.MvcApi.service;
-//
-//import com.api.MvcApi.domain.User;
-//
-//import java.util.ArrayList;
-//
-//public interface UserService {
-//    // 모든 유저 조회
-//    ArrayList<User> findAll();
-//    // 특정 유저 조회
-//    User findById(String id);
-//    // 유저 생성
-//    User createUser(User newUser);
-//    // 유저 수정
-//    void updateUsername(String id, String newUsername);
-//    // 유저 삭제
-//    void deleteUser(String id);
-//
-//}
+package com.api.MvcApi.service;
+
+import com.api.MvcApi.domain.User;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public interface UserService {
+    // 모든 유저 조회
+    List<User> findAll();
+    // 특정 유저 조회
+    User findById(String id);
+    // 유저 생성
+    User createUser(User newUser);
+    // 유저 수정
+    void updateUsername(@RequestBody String id, @RequestBody User user);
+    // 유저 삭제
+    void deleteUser(String id);
+
+}
--- src/main/java/com/api/MvcApi/service/UserServiceImpl.java
+++ src/main/java/com/api/MvcApi/service/UserServiceImpl.java
@@ -1,72 +1,59 @@
-//package com.api.MvcApi.service;
-//
-//import com.api.MvcApi.domain.User;
-//import com.api.MvcApi.repository.UserRepository;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//
-//@Slf4j
-//@Service
-//public class UserServiceImpl implements UserService {
-//    private final UserRepository userRepo;
-//
-//    @Autowired
-//    public UserServiceImpl(UserRepository userRepository) {
-//        this.userRepo = userRepository;
-//    }
-//
-//    @Override
-//    public ArrayList<User> findAll() {
-//        ArrayList<User> userList = new ArrayList<>();
-//        try{
-//            userList = userRepo.findAll();
-//        } catch (SQLException e) {
-//            log.error("findAll service error = {}", e);
-//        }
-//        return userList;
-//    }
-//
-//    @Override
-//    public User findById(String id) {
-//        User user = new User();
-//        try {
-//            user = userRepo.findById(id);
-//        } catch (SQLException e) {
-//            log.error("findById service error = {}", e);
-//        }
-//        return user;
-//    }
-//
-//    @Override
-//    public User createUser(User newUser) {
-//        User user = new User();
-//        try {
-//            user = userRepo.createUser(newUser);
-//        } catch (SQLException e) {
-//            log.error("createUser service error = {}", e);
-//        }
-//        return user;
-//    }
-//
-//    @Override
-//    public void updateUsername(String id, String newUsername) {
-//        try {
-//            userRepo.updateUsername(id, newUsername);
-//        } catch (SQLException e) {
-//            log.error("updateUserName service error = {}", e);
-//        }
-//    }
-//
-//    @Override
-//    public void deleteUser(String id) {
-//        try {
-//            userRepo.deleteUser(id);
-//        } catch (SQLException e) {
-//            log.error("deleteUser service error = {}", e);
-//        }
-//    }
-//}
+package com.api.MvcApi.service;
+
+import com.api.MvcApi.domain.User;
+import com.api.MvcApi.domain.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepo = userRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    @Override
+    public User findById(String id) {
+        return userRepo.findById(id).orElse(null);
+    }
+
+    @Override
+    public User createUser(User newUser) {
+        return userRepo.save(newUser);
+    }
+
+    @Override
+    public void updateUsername(@RequestBody String id, @RequestBody User user) {
+        Optional<User> updateUser = userRepo.findById(id);
+
+        updateUser.ifPresent(selectUser -> {
+            selectUser.setId(user.getId());
+            selectUser.setUsername(user.getUsername());
+            selectUser.setPassword(user.getPassword());
+
+            userRepo.save(selectUser);
+        });
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        Optional<User> updateUser = userRepo.findById(id);
+
+        updateUser.ifPresent(selectUser -> {
+            userRepo.delete(selectUser);
+        });
+    }
+}
