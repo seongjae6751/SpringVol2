1481fdfc6b0d571ef8caa69fcfcf945017e4df33
김성재 <103095432+seongjae6751@users.noreply.github.com>
2024-01-01 22:33:20 +0900

    JPA TEST

--- src/main/java/com/api/MvcApi/domain/User.java
+++ src/main/java/com/api/MvcApi/domain/User.java
@@ -1,14 +1,18 @@
 package com.api.MvcApi.domain;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
+@Entity // 테이블과 링크될 클래스임을 나타낸다. camel case 이름을 언어스코어 네이밍으로 테이블 이름을 정함
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User {
+    @Id // 해당 테이블의 PK 필드를 나타낸다
+    // @GeneratedValue // PK 생성 규칙을 나타냄 이 어노테이션을 추가시에 auto_increment가 된다.
     private String id;
+    // @Column(name = DB에 있는 필드 이름) -> DB 테이블 빌드 명과 Entity클래스 필드 명이 다를 경우
     private String username;
     private String password;
 }
--- src/test/java/com/api/MvcApi/MvcApiApplicationTests.java
+++ src/test/java/com/api/MvcApi/MvcApiApplicationTests.java
@@ -1,13 +1,26 @@
 package com.api.MvcApi;
 
+import com.api.MvcApi.domain.User;
+import com.api.MvcApi.domain.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MvcApiApplicationTests {
+public class MvcApiApplicationTests {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
-	void contextLoads() {
+	public void create() {
+		User user = new User();
+
+		user.setId("test");
+		user.setUsername("jpaTest");
+		user.setPassword("123");
+
+		User newUser = userRepository.save(user);
 	}
 
 }
